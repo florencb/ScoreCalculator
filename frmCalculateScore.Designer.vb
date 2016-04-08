e986d0bba90f9f904d3e8fe1cc8856a5884b85a1
florencb <bradjflorence@gmail.com>
2016-04-08 13:07:10 -0400

    initial commit

---
+++ frmCalculateScore.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalculateScore
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtScore = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.txtCount = New System.Windows.Forms.TextBox()
+        Me.txtAverage = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(163, 10)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "&Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(53, 160)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "C&lear Scores"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(163, 160)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Score:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Score total:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(19, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Score count:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(19, 122)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Average"
+        '
+        'txtScore
+        '
+        Me.txtScore.Location = New System.Drawing.Point(86, 10)
+        Me.txtScore.Name = "txtScore"
+        Me.txtScore.Size = New System.Drawing.Size(63, 20)
+        Me.txtScore.TabIndex = 0
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(86, 45)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.ReadOnly = True
+        Me.txtTotal.Size = New System.Drawing.Size(63, 20)
+        Me.txtTotal.TabIndex = 1
+        '
+        'txtCount
+        '
+        Me.txtCount.Location = New System.Drawing.Point(86, 80)
+        Me.txtCount.Name = "txtCount"
+        Me.txtCount.ReadOnly = True
+        Me.txtCount.Size = New System.Drawing.Size(63, 20)
+        Me.txtCount.TabIndex = 2
+        '
+        'txtAverage
+        '
+        Me.txtAverage.Location = New System.Drawing.Point(86, 115)
+        Me.txtAverage.Name = "txtAverage"
+        Me.txtAverage.ReadOnly = True
+        Me.txtAverage.Size = New System.Drawing.Size(63, 20)
+        Me.txtAverage.TabIndex = 10
+        '
+        'frmCalculateScore
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(258, 199)
+        Me.Controls.Add(Me.txtAverage)
+        Me.Controls.Add(Me.txtCount)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtScore)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Name = "frmCalculateScore"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Score Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtScore As System.Windows.Forms.TextBox
+    Friend WithEvents txtTotal As System.Windows.Forms.TextBox
+    Friend WithEvents txtCount As System.Windows.Forms.TextBox
+    Friend WithEvents txtAverage As System.Windows.Forms.TextBox
+
+End Class
---
+++ frmCalculateScore.vb
@@ -0,0 +1,49 @@
+﻿Public Class frmCalculateScore
+    Dim score As Integer
+    Dim sum As Integer
+    Dim counts As Integer
+    Dim average As Decimal
+
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim score As Integer = CInt(txtScore.Text)
+        sum += score
+        counts = counts + 1
+
+        txtTotal.Text = sum.ToString
+        txtCount.Text = counts.ToString
+        average = CDec(sum / counts)
+        txtAverage.Text = (Math.Round(average, 0, MidpointRounding.AwayFromZero).ToString)
+
+
+
+        txtScore.Select()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        score = 0
+        sum = 0
+        counts = 0
+        average = 0
+
+        txtAverage.Text = ""
+        txtCount.Text = ""
+        txtScore.Text = ""
+        txtTotal.Text = ""
+
+
+        For Each txt As Control In Me.Controls
+            If TypeOf txt Is TextBox Then
+                txt.Text = String.Empty
+            End If
+        Next
+
+        txtScore.Select()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
+
